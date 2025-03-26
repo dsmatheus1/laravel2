9a3c64030764b2a2733c1435c6322b3be3261137
dsmatheus1 <matheussousamedeiros45@gmail.com>
2025-03-26 18:24:53 -0300

    teste

--- resources/views/_partials/menu.blade.php
+++ resources/views/_partials/menu.blade.php
@@ -16,7 +16,7 @@
                         <a class="nav-link" href="#">Adotar</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Contatoo</a>
+                        <a class="nav-link" href="#">Contato</a>
                     </li>
                 </ul>
             </div>
